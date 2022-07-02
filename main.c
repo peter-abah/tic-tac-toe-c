367128000ee10e34d4d627e3d44299b1d2455a63
Peter Abah <peterabah.ob@gmail.com>
2022-07-02 11:47:32 +0000

    Add reset_board function

--- main.c
+++ main.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
+#define BOARD_SIZE 3
+
+void reset_board(char (*board)[BOARD_SIZE]);
+
 int main(void) {
+    char board[BOARD_SIZE][BOARD_SIZE];
+    reset_board(board);
+
     printf("TIC TAC TOE\n");
+}
+
+void reset_board(char (*board)[BOARD_SIZE]) {
+    for (int i = 0; i < BOARD_SIZE; i++) {
+        for (int j = 0; j < BOARD_SIZE; j++) {
+            board[i][j] = -1;
+        }
+    }
 }
