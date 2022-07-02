82f004e7e3921628120905cddf127759c16b986f
Peter Abah <peterabah.ob@gmail.com>
2022-07-02 17:37:55 +0000

    Add play_game function

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
+    #include <ctype.h>
 #include <stdio.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -9,20 +11,118 @@ typedef struct {
     int y;
 } position;
 
+void clear_stdin(void);
 position *get_input_pos(char (*board)[BOARD_SIZE]);
 int get_int(char *prompt, int min, int max);
+bool check_winner(char (*board)[BOARD_SIZE], position *last_pos);
 void print_board(char (*board)[BOARD_SIZE]);
+void play_game(char (*board)[BOARD_SIZE]);
 void reset_board(char (*board)[BOARD_SIZE]);
 
 
 int main(void) {
     char board[BOARD_SIZE][BOARD_SIZE];
-    reset_board(board);
+    char c;
 
-    print_board(board);
-    position *pos = get_input_pos(board);
-    printf("%i %i\n", pos->x, pos->y);
-    free(pos);
+    do {
+        reset_board(board);
+        play_game(board);
+
+        // Ask user if they want to play again
+        printf("Play again?(y/n) ");
+        c = getchar();
+
+        // Clear new line
+        clear_stdin();
+    } while (tolower(c) == 'y');
+}
+
+void play_game(char (*board)[BOARD_SIZE]) {
+    char current = 'x';
+    int cell_no = BOARD_SIZE * BOARD_SIZE;
+
+    int i;
+    // Loop nine times and after that all cells are filled so draw
+    for (i = 0; i < cell_no; i++) {
+        printf("\n");
+        print_board(board);
+        printf("\n");
+
+        printf("Player %c turn\n", current);
+        position *pos = get_input_pos(board);
+        board[pos->y][pos->x] = current;
+
+        if (check_winner(board, pos)) {
+            free(pos);
+            printf("\nPlayer %c wins!\n\n", current);
+            break;
+        }
+
+        // Change players
+        current = current == 'x' ? 'o' : 'x';
+        free(pos);
+        printf("\n");
+    }
+
+    // if the for loop runs to 9 then draw
+    if (i == cell_no) {
+        print_board(board);
+        printf("\nDRAW!\n\n");
+    }
+}
+
+bool check_winner(char (*board)[BOARD_SIZE], position *last_pos) {
+    char current = board[last_pos->y][last_pos->x];
+
+    // if count is equal to boarf size then player wins
+    int count = 0;
+
+    // Check row of pos
+    for (int i = 0; i < BOARD_SIZE; i++) {
+        if (board[last_pos->y][i] != current) {
+            break;
+        }
+        if (i == BOARD_SIZE - 1) {
+            return true;
+        }
+    }
+
+    //check column of pos
+    for (int i = 0; i < BOARD_SIZE; i++) {
+        if (board[i][last_pos->x] != current) {
+            break;
+        }
+        if (i == BOARD_SIZE - 1) {
+            return true;
+        }
+    }
+
+    // Check diagonal
+    if (last_pos->x == last_pos->y) {
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            if (board[i][i] != current) {
+                break;
+            }
+            if (i == BOARD_SIZE - 1) {
+                return true;
+            }
+        }
+    }
+
+    // Check anti diagonal
+    if (last_pos->x + last_pos->y == BOARD_SIZE - 1) {
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            if (board[i][BOARD_SIZE - 1 - i] != current) {
+                break;
+            }
+            if (i == BOARD_SIZE - 1) {
+                return true;
+            }
+        }
+    }
+
+    // At this point there is no winner
+    return false;
 }
 
 
@@ -36,13 +136,13 @@ void reset_board(char (*board)[BOARD_SIZE]) {
 
 void print_board(char (*board)[BOARD_SIZE]) {
     // Print heading
-    printf("   0   1   2\n");
+    printf("     0   1   2\n");
     for (int i = 0; i < BOARD_SIZE; i++) {
         // Print horizontal separator
         if (i > 0) {
-            printf("  -+-+-+-+-+-\n");
+            printf("    -+-+-+-+-+-\n");
         }
-        printf("%i ", i);
+        printf("  %i ", i);
 
         for (int j = 0; j < BOARD_SIZE; j++) {
             // Print separator only if it is not the last cell on the row
@@ -66,8 +166,8 @@ position *get_input_pos(char (*board)[BOARD_SIZE]) {
     position *pos = malloc(sizeof(position));
 
     do {
-        pos->x = get_int("X: ", 0, BOARD_SIZE - 1);
         pos->y = get_int("Y: ", 0, BOARD_SIZE - 1);
+        pos->x = get_int("X: ", 0, BOARD_SIZE - 1);
     } while (board[pos->y][pos->x] != '\0');
 
     return pos;
@@ -75,7 +175,7 @@ position *get_input_pos(char (*board)[BOARD_SIZE]) {
 
 int get_int(char *prompt, int min, int max) {
     // Buffer to read int
-    char input[10];
+    char input[11];
     char *temp;
     int n;
 
@@ -83,6 +183,11 @@ int get_int(char *prompt, int min, int max) {
         printf("%s", prompt);
         // Get input
         fgets(input, sizeof(input), stdin);
+
+        // Clear input buffer if there is no new line
+        if (strchr(input, '\n') == NULL) {
+            clear_stdin();
+        }
 
         // Remove newline at end
         input[strcspn(input, "\n")] = 0;
@@ -96,3 +201,8 @@ int get_int(char *prompt, int min, int max) {
     return n;
 }
 
+// Clears stdin buffer
+void clear_stdin(void) {
+    char c;
+    while((c = getchar()) != '\n' && c != EOF) {}
+}
